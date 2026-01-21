aaf8145b10df91ad1180b8625c1fd9abdaa33ed0
AnvarIlyasov <ilyasov2007anvar@yandex.ru>
2026-01-21 19:21:20 +0300

    Delete tests directory

--- tests/test_calculator.cpp
+++
@@ -1,8 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-#include "calculator.h"
-
-TEST_CASE("Remainder works") {
-    REQUIRE(remainder(10, 3) == 1);
-    REQUIRE(remainder(20, 5) == 0);
-    REQUIRE(remainder(7, 4) == 3);
-}
